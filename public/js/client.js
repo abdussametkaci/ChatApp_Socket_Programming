b78831d3815fc1a3b99a1cc1ee80d9c29367ac1e
Abdussamet KACI <abdussamet.kaci@stu.fsm.edu.tr>
2021-05-16 20:15:02 +0300

    Added Emoji Picker

--- public/js/client.js
+++ public/js/client.js
@@ -4,3 +4,17 @@ let port = 3000
 let ip = "127.0.0.1"
 socket.connect('http://' + ip + ':' + port)
 
+
+
+let clicked = false
+document.getElementById('emoji-table').style.display="none"
+document.getElementById('btn-emoji').onclick = () => {
+    if(clicked){
+        document.getElementById('emoji-table').style.display="none"
+        clicked = false
+    } else {
+        document.getElementById('emoji-table').style.display="inline-block"
+        clicked = true
+    }
+}
+
